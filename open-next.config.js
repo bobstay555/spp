592a09a3f0a3bce2baaada9b1c9daae14fd812ff
smilepicphotography03-debug <smilepicphotography03@gmail.com>
2025-11-20 18:04:41 +0530

    Fix: Added required default config for OpenNext

---
+++ open-next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
+const config = {
+  // ... other configuration ...
+  
+  // This is the required section that cannot be empty.
+  config: {
+    default: {},
+  },
+};
+
+module.exports = config;
