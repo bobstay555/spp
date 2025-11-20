9f75eb5fe9161c3bd2e67d9d43b72ade7292b875
smilepicphotography03-debug <smilepicphotography03@gmail.com>
2025-11-20 15:25:22 +0530

    Fix: Add proper CommonJS OpenNext config for Cloudflare

---
+++ open-next.config.cjs
@@ -0,0 +1,12 @@
+﻿/** OpenNext Cloudflare config (CommonJS) */
+const config = {
+  nextjs: {
+    disableESLint: true,
+    disableTypeScript: true,
+  },
+  cloudflare: {
+    outputDir: ".open-next",
+  },
+};
+
+module.exports = config;
