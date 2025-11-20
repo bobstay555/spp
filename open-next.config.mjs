a512525a2818c4797c0ae57ae7933ba3ead8df8b
smilepicphotography03-debug <smilepicphotography03@gmail.com>
2025-11-20 16:31:04 +0530

    Fix OpenNext config using UTF-8 mjs file

---
+++ open-next.config.mjs
@@ -0,0 +1,12 @@
+/** OpenNext config */
+const config = {
+  nextjs: {
+    disableESLint: true,
+    disableTypeScript: true,
+  },
+  cloudflare: {
+    outputDir: ".open-next",
+  },
+};
+
+export default config;
