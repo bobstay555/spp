a9f8294d02967d324d777f3cc973c8d9fb50de9c
smilepicphotography03-debug <smilepicphotography03@gmail.com>
2025-11-20 17:05:20 +0530

    Fix: use JS OpenNext config for Cloudflare

---
+++ open-next.config.js
@@ -0,0 +1,10 @@
+/** Cloudflare OpenNext v1 config */
+module.exports = {
+  nextjs: {
+    disableESLint: true,
+    disableTypeScript: true,
+  },
+  cloudflare: {
+    outputDir: ".open-next",
+  },
+};
