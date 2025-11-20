06bf473ab2957a0efe2158a158a12e26ac9b6303
smilepicphotography03-debug <smilepicphotography03@gmail.com>
2025-11-20 17:59:26 +0530

    Fix: Cloudflare OpenNext v3 required TS config

---
+++ open-next.config.ts
@@ -0,0 +1,13 @@
+﻿import type { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+  nextjs: {
+    disableESLint: true,
+    disableTypeScript: true,
+  },
+  cloudflare: {
+    outputDir: ".open-next",
+  },
+};
+
+export default config;
